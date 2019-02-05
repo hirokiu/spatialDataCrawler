7614f6a43967351db0f3b5d62bf07218890f194f
Hiroki UEMATSU <uematsu.hiroki@gmail.com>
2019-02-05 14:31:02 +0900

    conflict merged.

--- service/instagram/getCityTag.py
+++ service/instagram/getCityTag.py
@@ -20,7 +20,7 @@ max_id = ''
 #max_id = 'AQD3njx3Hdjw5KtBTF0gw1pSHj4i1rwpGXBdHn6B3gVbvRdPRNcmy87f1vFibsoNBua2FrtQamuhHBTK1cnjD3VcDYh2YfMxRSNP5LzfI-V_wQ'
 
 url = "https://www.instagram.com/explore/tags/" + hashtag + "/?__a=1"
-base_dir = "/home/hiroki/SpatialKG/ranking_cities/{area}/".format(area=hashtag)
+base_dir = "./data_cities/{area}/".format(area=hashtag)
 filename = "{dir}/{area}_{max_id}.json".format(dir=base_dir,area=hashtag,max_id=0)
 #filename = "{dir}/{area}_{max_id}.json".format(dir=base_dir,area=hashtag,max_id=max_id)
 
@@ -30,6 +30,7 @@ res = requests.get(url, headers=headers)
 
 max_files = 10000
 files = 0
+=======
 
 while res.status_code == 200 :
     # TODO
@@ -42,31 +43,28 @@ while res.status_code == 200 :
     with open(filename, mode='w') as f:
         f.write(res.text)
 
-    files+=1
+    try:
+        # find next url
+        timelines = json.loads(res.text)
 
-    # find next url
-    # Content-Length分をread  
-    #json_text = res.read(length)  
+        if timelines['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['has_next_page'] :
+            max_id = timelines['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor']
+            filename = "{dir}/{area}_{max_id}.json".format(dir=base_dir,area=hashtag,max_id=max_id)
+        else :
+            max_id = None;
+            filename = "{dir}/{area}_last.json".format(dir=base_dir,area=hashtag)
+
+        sleep(3)
+
+        if max_id is not None :
+            print("NEXT call : " + str(max_id))
+            res = requests.get(url + "&max_id=" + str(max_id), headers = headers)
+        else :
+            res.status_code = 404
         
-    timelines = json.loads(res.text)
-    #timelines = json.loads(json_text)
+        print(url + "&max_id=" + str(max_id) + " : " +res.status_code)
 
-    if timelines['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['has_next_page'] :
-        max_id = timelines['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor']
-        filename = "{dir}/{area}_{max_id}.json".format(dir=base_dir,area=hashtag,max_id=max_id)
-    else :
-        max_id = None;
-        filename = "{dir}/{area}_last.json".format(dir=base_dir,area=hashtag)
-
-    sleep(3)
-    if max_id is not None :
-        print("NEXT call : " + url + "&max_id=" + str(max_id))
-        res = requests.get(url + "&max_id=" + str(max_id), headers = headers)
-        #headers_data = res.info()  
-        # ヘッダーからContent-Lengthを取得  
-        #json_length = int(headers_data.getheader('Content-Length', -1))  
-        #print(json_length)
-    else :
-        res.status_code = 404
-
-print(url + "&max_id=" + str(max_id) + " : " +res.status_code)
+    except Exception:
+        #import traceback
+        #traceback.print_exc()
+        pass
